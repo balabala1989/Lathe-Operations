43edcac6c0a47fdfed3bdbfedc1522b35845a918
Balaji Rajagopal <balabala1989@gmail.com>
2019-07-07 13:17:44 +0530

    Adding App Code

---
+++ src/mypackage/LatheOperations.java
@@ -0,0 +1,29 @@
+package mypackage;
+
+import net.rim.device.api.ui.UiApplication;
+
+/**
+ * This class extends the UiApplication class, providing a graphical user interface.
+ */
+public class LatheOperations extends UiApplication {
+    /**
+     * Entry point for application
+     * 
+     * @param args
+     *            Command line arguments (not used)
+     */
+    public static void main( String[] args ) {
+        // Create a new instance of the application and make the currently
+        // running thread the application's event dispatch thread.
+        LatheOperations theApp = new LatheOperations();
+        theApp.enterEventDispatcher();
+    }
+
+    /**
+     * Creates a new LatheOperations object
+     */
+    public LatheOperations() {
+        // Push a screen onto the UI stack for rendering.
+        pushScreen( new LatheOperationsMainScreen() );
+    }
+}
